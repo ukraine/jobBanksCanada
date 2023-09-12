f6e22294e2a437882622641bbe645250538c6efe
Yuriy <events@>
2023-09-12 11:02:34 -0400

    fixed issue with the options page keeping opening

--- jobbank/background.js
+++ jobbank/background.js
@@ -1,12 +1,8 @@
-browser.runtime.onStartup.addListener(() => {
-   browser.runtime.openOptionsPage();
- });
- 
- // Listen for the extension being installed or updated
+
+// Listen for the extension being installed or updated
 browser.runtime.onInstalled.addListener(function(details) {
    if (details.reason === 'install') {
      // Open the options page on first install
      browser.runtime.openOptionsPage();
    }
  });
- 
